0c20adc00a40c8bfac3242aa0bbe39300bd86bbe
Greg Richardson <gregnr@uvic.ca>
2015-07-12 16:52:05 -0700

    Abstracted web endpoints to key-value object

--- server.js
+++ server.js
@@ -1,34 +1,52 @@
+var util = require("util");
 var express = require("express");
 var app = express();
 var pg = require("pg");
 
 var conString = "postgres://monitor:monitor@localhost/monitor";
 
-app.get("/api/v1/hostCount/", function (req, res) {
+var endpointQueries = {
+    "hostCount": "SELECT host, COUNT(*) AS count FROM MessageExchange GROUP BY host ORDER BY count DESC;"
+};
 
-    pg.connect(conString, function(err, client, done) {
+//Loop through endpoints and register them with express app
+for (var endpoint in endpointQueries) {
 
-        if (err) {
-            console.error("Could not connect to postgres", err);
-            return;
-        }
+    //Make sure the key doesn't come from the object prototype
+    if (endpointQueries.hasOwnProperty(endpoint)) {
+    
+        var queryString = endpointQueries[endpoint];
         
-        var queryString = "SELECT host, COUNT(*) AS count FROM MessageExchange GROUP BY host ORDER BY count DESC;"
-      
-        client.query(queryString, function(err, result) {
-        
-            done();
-            
-            if (err) {
-                console.error("Error running query", err);
-                return;
-            }
+        //Add endpoint to express app
+        app.get(util.format("/api/v1/%s/", endpoint), function (req, res) {
 
-            res.send(JSON.stringify(result.rows));
+            //Connect to PostgreSQL database
+            pg.connect(conString, function(err, client, done) {
+
+                if (err) {
+                    console.error("Could not connect to postgres", err);
+                    return;
+                }
+                
+                //Perform the query
+                client.query(queryString, function(err, result) {
+                
+                    done();
+                    
+                    if (err) {
+                        console.error("Error running query", err);
+                        return;
+                    }
+                    
+                    //Return rows as JSON to the client
+                    res.send(JSON.stringify(result.rows));
+                });
+            });
         });
-    });
-});
+    }
+}
 
+//Start express app
 var server = app.listen(80, function () {
 
   var host = server.address().address;
