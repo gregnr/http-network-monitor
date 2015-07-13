2e4464de44c0f588ecf147db83b7332c62e7204b
Greg Richardson <gregnr@uvic.ca>
2015-07-12 17:31:15 -0700

    Added more endpoints and queries

--- server.js
+++ server.js
@@ -6,44 +6,53 @@ var pg = require("pg");
 var conString = "postgres://monitor:monitor@localhost/monitor";
 
 var endpointQueries = {
-    "hostCount": "SELECT host, COUNT(*) AS count FROM MessageExchange GROUP BY host ORDER BY count DESC;"
+    "domains": "SELECT (regexp_matches(host, '[^.]+.[^.]+$'))[1] as domain, COUNT(*) AS total_requests FROM MessageExchange GROUP BY domain ORDER BY total_requests DESC limit 20;",
+    "contenttypes": "SELECT content_type, count(*) AS total_responses FROM MessageExchange WHERE content_type IS NOT NULL GROUP BY content_type ORDER BY total_responses DESC;",
+    "webservers": "SELECT server, count(*) AS total_responses FROM MessageExchange WHERE server IS NOT NULL GROUP BY server ORDER BY total_responses DESC;",
+    "imagetypes": "SELECT (regexp_matches(content_type,'image/(.*)'))[1] as image_type, count(*) AS total FROM MessageExchange GROUP BY content_type ORDER BY total DESC;",
+    "useragents": "SELECT user_agent, COUNT(*) AS total FROM MessageExchange GROUP BY user_agent ORDER BY total DESC limit 20;"
 };
 
 //Loop through endpoints and register them with express app
 for (var endpoint in endpointQueries) {
 
-    //Make sure the key doesn't come from the object prototype
-    if (endpointQueries.hasOwnProperty(endpoint)) {
+    //Preserve scope of endpoint
+    (function(endpoint) {
     
-        var queryString = endpointQueries[endpoint];
+        //Make sure the key doesn't come from the object prototype
+        if (endpointQueries.hasOwnProperty(endpoint)) {
         
-        //Add endpoint to express app
-        app.get(util.format("/api/v1/%s/", endpoint), function (req, res) {
+            var queryString = endpointQueries[endpoint];
+            
+            //Add endpoint to express app
+            app.get(util.format("/api/v1/%s/", endpoint), function (req, res) {
 
-            //Connect to PostgreSQL database
-            pg.connect(conString, function(err, client, done) {
+                //Connect to PostgreSQL database
+                pg.connect(conString, function(err, client, done) {
 
-                if (err) {
-                    console.error("Could not connect to postgres", err);
-                    return;
-                }
-                
-                //Perform the query
-                client.query(queryString, function(err, result) {
-                
-                    done();
-                    
                     if (err) {
-                        console.error("Error running query", err);
+                        console.error("Could not connect to postgres", err);
                         return;
                     }
                     
-                    //Return rows as JSON to the client
-                    res.send(JSON.stringify(result.rows));
+                    //Perform the query
+                    client.query(queryString, function(err, result) {
+                    
+                        done();
+                        
+                        if (err) {
+                            console.error("Error running query", err);
+                            return;
+                        }
+                        
+                        //Return rows as JSON to the client
+                        res.send(JSON.stringify(result.rows));
+                    });
                 });
             });
-        });
-    }
+        }
+        
+    })(endpoint);
 }
 
 //Start express app
