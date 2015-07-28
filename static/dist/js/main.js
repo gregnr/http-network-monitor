341043e27689cba2c32c10540d9cf697012c57e9
Jeremy Kroeker <PolloDiablo@users.noreply.github.com>
2015-07-28 01:09:46 -0700

    Added a few more queries

--- static/dist/js/main.js
+++ static/dist/js/main.js
@@ -1,10 +1,27 @@
-var createPieChart = function(chartElement, data) {
+var createPieChart = function(chartElement, dataSet) {
+
+	// To avoid an over-filled chart, after a certain # of items,
+	//		group them into an "other" category
+	// This assumes a descending list order list
+	var maxElements = 20;
+	if(dataSet.length > maxElements + 1){
+		for(var i = 0; i < dataSet.length; i++){
+			if(i == maxElements){
+				dataSet[i].label = "Other";
+			}else if(i > maxElements){
+				// Sum any remaining elements under 'other'
+				dataSet[maxElements].data = parseInt(dataSet[maxElements].data) +  parseInt(dataSet[i].data);
+				dataSet[i].data = 0; // This will hide the actual value from the chart
+				dataSet[i].label = ""; // This will hide the actual value from the chart
+			}
+		}
+	}
 
     var labelFormatter = function(label) {
         return "<span style='color: #000000; padding-left: 5px;'>" + label + "<span/>";
     };
     
-    var plotObj = $.plot(chartElement, data, {
+    var plotObj = $.plot(chartElement, dataSet, {
         series: {
             pie: {
                 show: true,
@@ -33,7 +50,9 @@ var createPieChart = function(chartElement, data) {
 
 $(document).ready(function() {
 
-    //Load domain chart
+	//Following the numbering convention from the Google Doc :)
+
+    //1. Load domain chart
     $.ajax({
         url: "../api/v1/domains/",
         dataType: "json"
@@ -52,7 +71,47 @@ $(document).ready(function() {
     });
     
     
-    //Load content type chart
+    //2. Load hourly traffic chart
+    $.ajax({
+        url: "../api/v1/hourlytraffic/",
+        dataType: "json"
+    }).then(function(dataSet) {
+        
+        // The input dataset may not have a value for every hour..
+        // 	Nicely format a new data set which has a value for each hour
+        //	Rightmost bar of chart is the current time (t0), count backwards from there
+        var fullDataSet = new Array(24);
+        var currentHour = new Date().getHours();
+        for(var i = 0 ; i <24 ; i ++){
+        	var difference = i-23;
+        	var actual_hr = (currentHour + difference)%24;
+        	if(actual_hr < 0 ){
+        		actual_hr += 24;
+        	}
+        	// Check for this hour in the actual data
+        	var ctl = 0;
+        	for(var j = 0 ; j <dataSet.length ; j ++){
+        		if(dataSet[j].hour == actual_hr){
+        			ctl = dataSet[j].content_length_total;
+        		}
+        	}
+       		fullDataSet[i] =  {hour: difference, content_length_total: ctl};
+        }
+        
+        Morris.Bar({
+            element: 'hourly-traffic-bar-chart',
+            data: fullDataSet,
+            xkey: 'hour',
+            ykeys: ['content_length_total'],
+            labels: ['Number of bytes'],
+            hideHover: 'auto',
+            gridTextSize: 8,
+            resize: true
+        });
+    });
+    
+    
+    //3. Load content type chart
     $.ajax({
         url: "../api/v1/contenttypes/",
         dataType: "json"
@@ -77,7 +136,7 @@ $(document).ready(function() {
     });
     
     
-    //Load server chart
+    //5. Load server chart
     $.ajax({
         url: "../api/v1/webservers/",
         dataType: "json"
@@ -96,7 +155,7 @@ $(document).ready(function() {
     });
     
     
-    //Load image type chart
+    //6. Load image type chart
     $.ajax({
         url: "../api/v1/imagetypes/",
         dataType: "json"
@@ -119,4 +178,50 @@ $(document).ready(function() {
         
         createPieChart(chartElement, flotData);
     });
+
+
+    //10. Load data by domain chart
+    $.ajax({
+        url: "../api/v1/domaindata/",
+        dataType: "json"
+    }).then(function(data) {
+        
+        Morris.Bar({
+            element: 'domain-data-bar-chart',
+            data: data,
+            xkey: 'domain',
+            ykeys: ['content_length_total'],
+            labels: ['Number of bytes'],
+            hideHover: 'auto',
+            gridTextSize: 8,
+            resize: true
+        });
+    });
+    
+
+    //12. Load user agent chart
+    $.ajax({
+        url: "../api/v1/useragents/",
+        dataType: "json"
+    }).then(function(data) {
+        
+        var flotData = [];
+        
+        //Convert data to flot format
+        for (var i = 0; i < data.length; i++) {
+            
+            var row = data[i];
+            
+            flotData.push({
+                label: row["user_agent"],
+                data: row["total"]
+            });
+        }
+        
+        var chartElement = $("#user-agent-pie-chart");
+        
+        createPieChart(chartElement, flotData);
+    });
+    
+
 });
